e05a61064653e951b62f62b0fbcf9211bff37227
NaveenMadhawa <naveenmadhawa2022@gmail.com>
2025-06-18 19:36:28 +0530

    Created AdminDashboard

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import CompanyMain from "./Components/CompanyComponents/CompanyMain"
 import AdvertismentPage from "./Components/GuestComponents/AdvertismentPage"
 
 
+
 function App() {
  
   return (
     <>
       {/* <AdvertismentPage/> */}
       <CompanyMain/>
+      
     </>
   )
 }

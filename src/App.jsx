d50a2747d32a89bde04a331f0d284204f1b82305
Dhanurathnayake <127535488+Dhanurathnayake@users.noreply.github.com>
2025-06-19 11:34:11 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,29 @@
-import CompanyMain from "./Components/CompanyComponents/CompanyMain"
-import AdvertismentPage from "./Components/GuestComponents/AdvertismentPage"
-
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import LoginPage from './Components/Login_Register/LoginPage';
+import RegisterPage from './Components/Login_Register/RegisterPage';
 
 function App() {
- 
   return (
-    <>
-      {/* <AdvertismentPage/> */}
-      <CompanyMain/>
-    </>
-  )
+    <Router>
+      <div className="App">
+        <Routes>
+          {/* Default route redirects to login */}
+          <Route path="/" element={<Navigate to="/LoginPage" replace />} />
+          
+          {/* Login page route */}
+          <Route path="/login" element={<LoginPage />} />
+          
+          {/* Registration page route */}
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/signup" element={<RegisterPage />} /> {/* Alternative route */}
+          
+          {/* Catch-all route for 404 - redirects to login */}
+          <Route path="*" element={<Navigate to="/login" replace />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;

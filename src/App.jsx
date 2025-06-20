0872087edeb815bc1fa6549801e44919ddd11691
kavishka07 <tvkrp325@gmail.com>
2025-06-20 11:36:54 +0530

    added user mangement

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,25 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import CompanyMain from './Components/CompanyComponents/CompanyMain';
 import AdvertismentPage from './Components/GuestComponents/AdvertismentPage';
+import Index from './Components/AdminComponent/UserManagement';
+
 
 
 function App() {
-  return (
+   return (
 
-    <Router>
-      <Routes>
-        <Route path="/" element={<AdvertismentPage />} />
-        <Route path="/company/*" element={<CompanyMain />} />
-        {/* <Route path="/student/*" element={<StudentMain />} /> */}
-      </Routes>
-    </Router>
+  <Router>
+    <Routes>
+      
+      <Route path="/" element={<AdvertismentPage />} />
+      <Route path="/company/*" element={<CompanyMain />} />
+      <Route path="/user/*" element={<Index />} />
+      {/* <Route path="/student/*" element={<StudentMain />} /> */}
+    </Routes>
+ </Router>
+    
   );
+
 
 }
 
